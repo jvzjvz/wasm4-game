f2ecc5d9e44cd4204fb7beaffe5cb1521ddc8128
JCodV <154752793+JCodV@users.noreply.github.com>
2026-07-23 22:01:40 -0400

    added guy idle sprite, will not have dark red because transparency took its place

--- src/main.zig
+++ src/main.zig
@@ -24,7 +24,9 @@ const vec2 = struct {
 };
 
 const Guy = struct {
-    const speed = 1.0;
+    const SPEED = 1.0;
+    const MAX_PUNCH_POWER = 100;
+    const PUNCH_GROWTH = 2;
 
     position: vec2,
     face_direction: Direction,
@@ -46,6 +48,7 @@ var prev_input: w4.Gamepad = undefined;
 var guy = std.mem.zeroInit(Guy, .{
     .face_direction = .Right
 });
+
 var obstacles: [MAX_OBSTACLES]Obstacle = undefined;
 
 export fn start() void {
@@ -84,15 +87,15 @@ export fn update() void {
     {
         const input = w4.gamepads[0];
         if (input.button_left) {
-            guy.position.x -= Guy.speed;
+            guy.position.x -= Guy.SPEED;
         } else if (input.button_right) {
-            guy.position.x += Guy.speed;
+            guy.position.x += Guy.SPEED;
         }
 
         if (input.button_up) {
-            guy.position.y -= Guy.speed;
+            guy.position.y -= Guy.SPEED;
         } else if (input.button_down) {
-            guy.position.y += Guy.speed;
+            guy.position.y += Guy.SPEED;
         }
 
         if (input.button_1) {
@@ -103,22 +106,51 @@ export fn update() void {
             guy.face_direction = .Left;
         }
 
+        if (input.button_1) {
+            guy.punch_power += Guy.PUNCH_GROWTH;
+            guy.punch_power = clamp(u8, guy.punch_power, 0, Guy.MAX_PUNCH_POWER);
+
+            const msg = std.fmt.allocPrint(arena.allocator(), "punch_power: {}", .{guy.punch_power}) catch "";
+            w4.trace(msg);
+        }
+
+        if (guy.punch_power != 0 and !input.button_1 and !prev_input.button_1) {
+            const msg = std.fmt.allocPrint(arena.allocator(), "released punch {}", .{guy.punch_power}) catch "";
+            w4.trace(msg);
+            guy.punch_power = 0;
+        }
+
         prev_input = input;
     }
 
     w4.text("Hello from Zig!", 10, 10);
 
-    w4.rect(@intFromFloat(guy.position.x), @intFromFloat(guy.position.y), 16, 16);
 
+
+    // guy draw
     {
+
+        const x: i32 = @intFromFloat(guy.position.x);
+        const y: i32 = @intFromFloat(guy.position.y);
+        // w4.rect(x, y, 16, 16);
+
+        w4.draw.* = .{
+            .color_1 = .transparent,
+            .color_2 = .palette_1,
+            .color_3 = .palette_3,
+            .color_4 = .palette_4,
+        };
+
+        w4.blit(&assets.guy_idle, guy.face_direction.value() * x, y, assets.guy_idle_width, assets.guy_idle_height, .{.format = .bpp_2});
+
         w4.draw.* = .{
             .color_1 = .palette_4,
             .color_2 = .palette_4,
         };
 
         const len = 40;
-        const x: i32 = @intFromFloat(guy.position.x);
-        const y: i32 = @intFromFloat(guy.position.y);
+        // const x: i32 = @intFromFloat(guy.position.x);
+        // const y: i32 = @intFromFloat(guy.position.y);
 
         if (guy.face_direction == .Left) {
             w4.line(x - len, y, x, y);
@@ -146,4 +178,10 @@ export fn update() void {
     w4.blit(&assets.ui, 1, 107, assets.ui_width, assets.ui_height, .{.format = .bpp_2});
 
     _ = arena.reset(.retain_capacity);
+}
+
+fn clamp(comptime T: type, value: T, lower: T, upper: T) T {
+    if (value < lower) return lower;
+    if (value > upper) return upper;
+    return value;
 }
